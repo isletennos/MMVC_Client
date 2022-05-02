fcf34fcb08d1d45dea3c6eae45fae467fa955751
isletennos <isle.tennos@gmail.com>
2022-05-03 01:53:45 +0900

    ノイズ録音のデバイスを指定できるように

--- python/rec_environmental_noise.py
+++ python/rec_environmental_noise.py
@@ -3,6 +3,46 @@ import pyaudio
 import wave
 import numpy as np
 import time
+import json
+
+class VCPrifile():
+  def __init__(self, **kwargs):
+    for k, v in kwargs.items():
+      if type(v) == dict:
+        v = VCPrifile(**v)
+      self[k] = v
+    
+  def keys(self):
+    return self.__dict__.keys()
+
+  def items(self):
+    return self.__dict__.items()
+
+  def values(self):
+    return self.__dict__.values()
+
+  def __len__(self):
+    return len(self.__dict__)
+
+  def __getitem__(self, key):
+    return getattr(self, key)
+
+  def __setitem__(self, key, value):
+    return setattr(self, key, value)
+
+  def __contains__(self, key):
+    return key in self.__dict__
+
+  def __repr__(self):
+    return self.__dict__.__repr__()
+
+def config_get(conf):
+    config_path = conf
+    with open(config_path, "r") as f:
+        data = f.read()
+    config = json.loads(data)
+    hparams = VCPrifile(**config)
+    return hparams
 
 def MakeWavFile():
     chunk = 1024
@@ -16,11 +56,30 @@ def MakeWavFile():
             continue
         break
 
+    while True:  # 無限ループ
+        print('「myprofile.json」のパスを入力してください。')
+        profile_path = input('>> ')
+        try:
+            if profile_path:
+                break
+            else:
+                print('ファイルが存在しません')
+                continue
+    
+        except ValueError:
+            # ValueError例外を処理するコード
+            print('パスを入力してください・')
+            continue
+    
+    params = config_get(profile_path)
+    print(params.device.input_device1)
+
     p = pyaudio.PyAudio()
     stream = p.open(format = pyaudio.paInt16,
                     channels = 1,
                     rate = sr,
                     input = True,
+                    input_device_index=params.device.input_device1,
                     frames_per_buffer = chunk)
     #レコード開始
     print("あなたの環境ノイズを録音します。マイクの電源を入れて、何もせずに待機していてください。")
