738178de842967ac614c141d7413bb0449e35833
isletennos <isle.tennos@gmail.com>
2022-05-04 22:05:05 +0900

    ピポッさんの奴をコミット

--- python/output_audio_device_list.py
+++ python/output_audio_device_list.py
@@ -4,12 +4,21 @@ import pyaudio
 def main():
     audio = pyaudio.PyAudio()
     audio_devices = list()
+    isInOut = ""
 
     # 音声デバイス毎のインデックス番号を一覧表示
     for x in range(0, audio.get_device_count()): 
         devices = audio.get_device_info_by_index(x)
-        audio_devices.append("Index : " + str(devices['index']) + "  デバイス名 : " + devices['name'] + "\n")
+        if devices['maxInputChannels'] > 0:
+            isInOut = isInOut + "入"
+        if devices['maxOutputChannels'] > 0:
+            isInOut = isInOut + "出"
+        isInOut = isInOut + "力："
         
+        audio_devices.append(isInOut + "Index : " + str(devices['index']) + "  デバイス名 : " + devices['name'] + "\n")
+        
+        isInOut = ""
+
     with open('audio_device_list.txt', 'w', encoding='utf-8') as f:
         f.writelines(audio_devices)
 
